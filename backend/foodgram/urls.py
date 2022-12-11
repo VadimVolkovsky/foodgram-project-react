544bf3a3c20cf3da358d0bfd49ccd90853937a18
vadim <vadimbor93@mail.ru>
2022-12-11 20:57:13 +0300

    test isort

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -2,8 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from recipes.views import IngredientViewSet, RecipeViewSet, TagViewSet
 from rest_framework import routers
+
+from recipes.views import IngredientViewSet, RecipeViewSet, TagViewSet
 from users.views import CustomUserViewSet
 
 router = routers.DefaultRouter()
