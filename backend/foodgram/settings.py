48c235359b71804810ce5710f5626594fdf50add
vadim <vadimbor93@mail.ru>
2022-12-12 14:04:41 +0300

    localization test

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
